20197648012f8b0a6f575be20fc80d193e84ec0f
Achyut Neupane <achyutkneupane@gmail.com>
2025-08-03 16:09:42 +0545

    Navbar color changed

--- resources/js/Components/Shared/Navbar.tsx
+++ resources/js/Components/Shared/Navbar.tsx
@@ -1,24 +1,24 @@
 import useTheme from "@/Hooks/useTheme";
-import { cn } from "@/Lib/Utils";
-import { SiGithub } from "@icons-pack/react-simple-icons";
+import {cn} from "@/Lib/Utils";
+import {SiGithub} from "@icons-pack/react-simple-icons";
 import {Moon, Sun, LaptopMinimal} from "lucide-react";
-import { motion } from "motion/react";
+import {motion} from "motion/react";
 import {Theme} from "@/Types/Enums";
 import {FC} from "react";
 
 const ThemeToggler = (props: {
     className?: string;
 }) => {
-    const { className } = props;
-    const { setTheme, theme } = useTheme();
+    const {className} = props;
+    const {setTheme, theme} = useTheme();
 
-    switch(theme) {
+    switch (theme) {
         case Theme.Light:
-            return <Sun className={className} onClick={() => setTheme(Theme.Dark)} />;
+            return <Sun className={className} onClick={() => setTheme(Theme.Dark)}/>;
         case Theme.Dark:
-            return <Moon className={className} onClick={() => setTheme(Theme.System)} />;
+            return <Moon className={className} onClick={() => setTheme(Theme.System)}/>;
         default:
-            return <LaptopMinimal className={className} onClick={() => setTheme(Theme.Light)} />;
+            return <LaptopMinimal className={className} onClick={() => setTheme(Theme.Light)}/>;
 
     }
 }
@@ -26,6 +26,10 @@ const ThemeToggler = (props: {
 const Navbar = () => {
     const appName = import.meta.env.VITE_APP_NAME || "Laravel Nepal";
     const iconClass = "cursor-pointer text-neutral-800 dark:text-neutral-300";
+    const {theme} = useTheme();
+    const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? Theme.Dark : Theme.Light;
+
+    const isDarkMode = theme === Theme.Dark || (theme === Theme.System && systemTheme === Theme.Dark);
 
     return (
         <motion.div
@@ -33,14 +37,31 @@ const Navbar = () => {
                 "shadow-input fixed inset-x-0 top-4 z-50 mx-auto max-w-7xl rounded-full lg:top-12",
                 "flex items-center justify-between space-x-4 px-12 py-6 bg-black/50",
             )}
-            initial={{ y: -20, backgroundColor: "#00000000", backdropFilter: "blur(0px)", WebkitBackdropFilter: "blur(0px)" }}
-            animate={{ y: 0, backgroundColor: "#00000050", backdropFilter: "blur(4px)", WebkitBackdropFilter: "blur(4px)" }}
-            transition={{ duration: 0.5 }}
+            initial={{
+                y: -20,
+                backgroundColor: "#00000000",
+                backdropFilter: "blur(0px)",
+                WebkitBackdropFilter: "blur(0px)"
+            }}
+            animate={
+                isDarkMode ? {
+                    y: 0,
+                    backgroundColor: "#00000050",
+                    backdropFilter: "blur(4px)",
+                    WebkitBackdropFilter: "blur(4px)"
+                } : {
+                    y: 0,
+                    backgroundColor: "#ffffff20",
+                    backdropFilter: "blur(4px)",
+                    WebkitBackdropFilter: "blur(4px)"
+                }
+            }
+            transition={{duration: 0.5}}
         >
             <h1 className="relative bg-gradient-to-b from-neutral-400 dark:from-neutral-300 to-neutral-800 dark:to-neutral-500 bg-clip-text text-3xl font-bold text-transparent">{appName}</h1>
             <div className="flex flex-row items-center justify-end gap-3">
-                <SiGithub className={iconClass} />
-                <ThemeToggler className={iconClass} />
+                <SiGithub className={iconClass}/>
+                <ThemeToggler className={iconClass}/>
             </div>
         </motion.div>
     );
