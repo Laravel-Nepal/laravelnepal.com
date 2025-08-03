b36abee89cec2104e235f65268c98fd6935f7520
Achyut Neupane <achyutkneupane@gmail.com>
2025-08-03 14:23:40 +0545

    feat: FrontWrapper and Utils added

---
+++ resources/js/Types/Types.ts
@@ -0,0 +1,6 @@
+import { ComponentProps, type ReactNode } from "react";
+
+export interface LayoutProps {
+    children: ReactNode;
+    title?: string;
+}
---
+++ resources/js/Wrappers/FrontWrapper.tsx
@@ -0,0 +1,32 @@
+import { LayoutProps } from "@/Types/Types";
+import { Head } from "@inertiajs/react";
+import { FC } from "react";
+import { Bounce, ToastContainer } from "react-toastify";
+
+const FrontWrapper: FC<LayoutProps> = (props) => {
+    const { children, title } = props;
+
+    return (
+        <>
+            <Head title={title} />
+            <ToastContainer
+                position="bottom-center"
+                autoClose={2000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss={false}
+                draggable={false}
+                pauseOnHover
+                theme="dark"
+                transition={Bounce}
+            />
+            <div>
+                {children}
+            </div>
+        </>
+    );
+};
+
+export default FrontWrapper;
