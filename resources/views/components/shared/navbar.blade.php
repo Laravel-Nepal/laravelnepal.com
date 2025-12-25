7161806cdbe105dd5cfab5e61a47e767118b718c
achyutkneupane <achyutkneupane@gmail.com>
2025-12-25 16:53:57 +0545

    feat: adds animated landing page and responsive navbar component

---
+++ resources/views/components/shared/navbar.blade.php
@@ -0,0 +1,12 @@
+<nav class="fixed bottom-8 left-1/2 -translate-x-1/2 z-100 w-auto">
+    <div class="dock rounded-full px-4 py-3 flex items-center gap-2">
+        <a href="{{ route('page.landingPage') }}" class="w-10 h-10 bg-laravel-red rounded-full flex items-center justify-center font-black text-xs hover:scale-110 transition">LN</a>
+        <div class="h-6 w-px bg-white/10 mx-2"></div>
+        <div class="flex gap-1">
+            <a href="#feed" class="nav-item active">Feed</a>
+            <a href="#authors" class="nav-item">Blogs</a>
+            <a href="#authors" class="nav-item">Authors</a>
+            <a href="#projects" class="nav-item">Explore</a>
+        </div>
+    </div>
+</nav>
