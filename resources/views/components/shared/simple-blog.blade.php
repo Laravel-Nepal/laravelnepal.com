24b2be756df72b4eec70144fa59251879df9d2fb
achyutkneupane <achyutkneupane@gmail.com>
2025-12-25 17:57:57 +0545

    feat: adds SimpleBlog component

---
+++ resources/views/components/shared/simple-blog.blade.php
@@ -0,0 +1,4 @@
+<a href="#" class="group border-b border-white/5 pb-6 last:border-0 last:pb-0">
+    <span class="text-[10px] text-zinc-500 font-mono block mb-2">2 DAYS AGO</span>
+    <h3 class="font-bold text-lg leading-tight group-hover:text-laravel-red transition">Khalti releases new Laravel SDK v3</h3>
+</a>
