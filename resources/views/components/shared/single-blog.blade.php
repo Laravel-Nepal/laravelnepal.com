1cc940bf64930583db34dbfc136d0b4a8bb590a1
achyutkneupane <achyutkneupane@gmail.com>
2025-12-25 17:55:40 +0545

    feat: adds SingleBlog component for displaying individual blog posts

---
+++ resources/views/components/shared/single-blog.blade.php
@@ -0,0 +1,7 @@
+<a href="#" class="glass-panel p-6 rounded-4xl hover:-translate-y-2 transition-transform">
+    <div class="h-40 bg-zinc-900 rounded-2xl mb-4 overflow-hidden relative">
+        <div class="absolute inset-0 bg-linear-to-tr from-laravel-red/20 to-transparent"></div>
+    </div>
+    <h4 class="font-bold text-xl mb-2">Testing with Pest</h4>
+    <p class="text-xs text-zinc-500">Why Nepalese teams are switching from PHPUnit.</p>
+</a>
