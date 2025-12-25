9ca0973a5267308031aa6bdff3c8a3c190aab02c
achyutkneupane <achyutkneupane@gmail.com>
2025-12-25 23:15:58 +0545

    style: updates footer text color for improved visibility

--- resources/views/components/shared/footer.blade.php
+++ resources/views/components/shared/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="py-20 border-t border-white/5 text-center" id="footer">
-    <p class="text-xs text-neutral-600 uppercase tracking-[0.4em] mb-4">Laravel Nepal</p>
+    <p class="text-xs text-neutral-400 uppercase tracking-[0.4em] mb-4">Laravel Nepal</p>
     <p class="max-w-2xl mx-auto text-sm text-neutral-500 px-6 uppercase leading-loose">
         Officially permitted by the Laravel team. <br />
         Not officially affiliated with or endorsed by Laravel.
