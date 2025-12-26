2ecac93ea35f95936fb9c3e06ee6ccb81d11b310
achyutkneupane <achyutkneupane@gmail.com>
2025-12-26 12:12:56 +0545

    feat: adds author details section in TipInfolist

--- app/Filament/Resources/Tips/Schemas/TipInfolist.php
+++ app/Filament/Resources/Tips/Schemas/TipInfolist.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace App\Filament\Resources\Tips\Schemas;
 
+use App\Filament\Resources\Authors\AuthorResource;
+use Filament\Infolists\Components\ImageEntry;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
+use Filament\Support\Enums\FontWeight;
+use Filament\Support\Enums\TextSize;
 
 final class TipInfolist
 {
@@ -21,9 +25,6 @@ final class TipInfolist
                     ->aside()
                     ->components([
                         TextEntry::make('title'),
-                        TextEntry::make('author_username')
-                            ->label('Username'),
-                        TextEntry::make('author.name'),
                         TextEntry::make('date')
                             ->label('Published at')
                             ->date(),
@@ -43,6 +44,33 @@ final class TipInfolist
                             ->markdown()
                             ->columnSpanFull(),
                     ]),
+                Section::make('Author')
+                    ->description('Details about the author')
+                    ->relationship('author')
+                    ->schema([
+                        ImageEntry::make('avatar')
+                            ->columnSpanFull()
+                            ->circular()
+                            ->placeholder('N/A')
+                            ->hiddenLabel(),
+                        TextEntry::make('bio')
+                            ->columnSpanFull()
+                            ->weight(FontWeight::Bold)
+                            ->size(TextSize::Large)
+                            ->placeholder('N/A')
+                            ->hiddenLabel(),
+                        TextEntry::make('name')
+                            ->placeholder('N/A'),
+                        TextEntry::make('username')
+                            ->url(fn (string $state): string => AuthorResource::getUrl('view', ['record' => $state]))
+                            ->placeholder('N/A'),
+                        TextEntry::make('email')
+                            ->url(fn (string $state): string => 'mailto:'.$state)
+                            ->placeholder('N/A'),
+                    ])
+                    ->columns()
+                    ->columnSpanFull()
+                    ->aside(),
             ]);
     }
 }
