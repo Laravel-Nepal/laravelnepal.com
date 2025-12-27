55204c52a4d80df588911e1876bb26601877874c
achyutkneupane <achyutkneupane@gmail.com>
2025-12-27 17:26:44 +0545

    style: adds full width to tips list grid container

--- resources/views/components/section/landing-page/tips-list.blade.php
+++ resources/views/components/section/landing-page/tips-list.blade.php
@@ -2,7 +2,7 @@
     title="Tips"
     class="container mt-18"
 >
-    <div class="grid grid-cols-1 lg:grid-cols-3 order-2 lg:order-3 gap-4 lg:gap-6">
+    <div class="grid grid-cols-1 lg:grid-cols-3 order-2 lg:order-3 gap-4 lg:gap-6 w-full">
         @foreach($tips as $tip)
             <x-shared.tip-component :$tip />
         @endforeach
