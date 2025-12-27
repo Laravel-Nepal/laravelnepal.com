c0fdd525d59a990f772f9121134ea0f287910d36
achyutkneupane <achyutkneupane@gmail.com>
2025-12-27 14:31:51 +0545

    feat: updates styling for featured and single blog components

--- resources/views/components/shared/featured-blog.blade.php
+++ resources/views/components/shared/featured-blog.blade.php
@@ -1,12 +1,12 @@
 <a href="#" class="block group relative rounded-4xl overflow-hidden aspect-video glass w-full">
-{{--    <img src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?auto=format&fit=crop&q=80&w=2000" class="absolute inset-0 w-full h-full object-cover -z-10 group-hover:scale-105 transition duration-700 opacity-60 grayscale group-hover:grayscale-0">--}}
+    <div class="absolute inset-0 bg-linear-to-tr from-neutral-400/60 group-hover:from-laravel-red/20 to-transparent"></div>
     <div class="absolute bottom-0 left-0 p-8 md:p-12">
         @foreach($post->tags as $tag)
             <span class="badge">
                 {{ ucwords($tag) }}
             </span>
         @endforeach
-        <h2 class="text-3xl md:text-5xl font-bold leading-tight mb-4 group-hover:underline decoration-laravel-red decoration-2 underline-offset-8">
+        <h2 class="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold leading-tight mb-4 group-hover:underline decoration-laravel-red decoration-2 underline-offset-8">
             {{ $post->title }}
         </h2>
         <p class="text-zinc-300 max-w-xl text-sm md:text-base">
