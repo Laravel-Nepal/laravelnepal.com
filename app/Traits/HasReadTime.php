80048915c92854c7f00272a200c1dc224b589268
achyutkneupane <achyutkneupane@gmail.com>
2026-01-03 18:18:03 +0545

    feat: adds HasReadTime trait for calculating read time of content

---
+++ app/Traits/HasReadTime.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
+/**
+ * @property string $contentColumn
+ * @property-read int $minutes_read
+ *
+ * @method string|null contentValue()
+ */
+trait HasReadTime
+{
+    public function getContent(): string
+    {
+        if (method_exists($this, 'contentValue') && $this->contentValue() !== null) {
+            return $this->contentValue();
+        }
+
+        return data_get($this, $this->contentColumn());
+    }
+
+    /**
+     * @return Attribute<int, null>
+     */
+    protected function minutesRead(): Attribute
+    {
+        /** @phpstan-var string $content */
+        $content = $this->getContent();
+
+        return Attribute::make(
+            get: fn (): int => max(1, (int) ceil(str_word_count(strip_tags($content ?? '')) / 200)),
+        );
+    }
+
+    /**
+     * @return Attribute<string, null>
+     */
+    protected function minutesReadText(): Attribute
+    {
+        $singular = $this->minutes_read <= 1;
+
+        return Attribute::make(
+            get: fn (): string => $this->minutes_read.' min'.($singular ? '' : 's').' read',
+        );
+    }
+
+    protected function contentColumn(): string
+    {
+        return property_exists($this, 'contentColumn')
+            ? $this->contentColumn
+            : 'image';
+    }
+}
