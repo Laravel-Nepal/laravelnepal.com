24bbf5c879efb8631dc44eaa5e5783bc1ee65372
achyutkneupane <achyutkneupane@gmail.com>
2026-01-03 19:57:19 +0545

    style: updates hover effects for blog post links

--- resources/views/components/shared/single-blog.blade.php
+++ resources/views/components/shared/single-blog.blade.php
@@ -1,9 +1,9 @@
-<a href="{{ route('page.post.view', $post) }}" class="glass p-6 rounded-4xl hover:-translate-y-2 transition-transform group">
+<a href="{{ route('page.post.view', $post) }}" class="glass p-6 rounded-4xl hover:-translate-y-2 transition-transform group/blog">
     <div class="h-40 bg-zinc-900 rounded-2xl mb-4 overflow-hidden relative">
         <div
             @class([
                 "flex justify-center items-center flex-wrap gap-4 h-full",
-                "bg-linear-to-tr from-laravel-red/20 group-hover:from-laravel-red/30 to-transparent"
+                "bg-linear-to-tr from-laravel-red/20 group-hover/blog:from-laravel-red/30 to-transparent"
             ])
         >
             @foreach($post->tags as $tag)
@@ -16,7 +16,7 @@
     <div class="text-neutral-400 text-xs font-mono uppercase tracking-widest mb-2">
         {{ $post->date->format('M d, Y') }}
     </div>
-    <h4 class="font-bold text-xl mb-2 group-hover:text-laravel-red transition">
+    <h4 class="font-bold text-xl mb-2 group-hover/blog:text-laravel-red transition">
         {{ $post->title }}
     </h4>
     <p class="text-zinc-400 text-xs font-bold">
