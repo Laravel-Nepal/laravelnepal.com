80eea42d8b804ab57f97f0010791c3d1fdca2a46
achyutkneupane <achyutkneupane@gmail.com>
2026-01-05 23:53:33 +0545

    feat: adds database configuration and title suffix to SEO settings

--- config/seo.php
+++ config/seo.php
@@ -7,4 +7,8 @@ use AchyutN\LaravelSEO\Models\SEO;
 return [
     'model' => SEO::class,
     'sitemap' => '/sitemap.xml',
+    'database' => config('database.default', 'mysql'),
+    'title' => [
+        'suffix' => sprintf(' - %s', config('app.name')),
+    ],
 ];
