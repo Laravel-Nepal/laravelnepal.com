d8c6bc984e3f066b0fca1d4dbc7b56045d8e74b6
achyutkneupane <achyutkneupane@gmail.com>
2026-01-16 23:23:38 +0545

    feat: adds News resource with management and table functionalities

---
+++ app/Filament/Resources/News/NewsResource.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Filament\Resources\News;
+
+use App\Filament\Resources\News\Pages\ManageNews;
+use App\Filament\Resources\Posts\PostResource;
+use App\Models\News;
+use BackedEnum;
+use Filament\Actions\DeleteAction;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class NewsResource extends Resource
+{
+    protected static ?string $model = News::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::RectangleStack;
+
+    protected static string|BackedEnum|null $activeNavigationIcon = Heroicon::OutlinedRectangleStack;
+
+    public static function infolist(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextEntry::make('post.title'),
+                TextEntry::make('deleted_at')
+                    ->dateTime()
+                    ->visible(fn (News $record): bool => $record->trashed()),
+                TextEntry::make('created_at')
+                    ->dateTime()
+                    ->placeholder('-'),
+                TextEntry::make('updated_at')
+                    ->dateTime()
+                    ->placeholder('-'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('post.title')
+                    ->url(fn (News $record): string => PostResource::getUrl('view', ['record' => $record->post]))
+                    ->searchable(),
+                TextColumn::make('created_at')
+                    ->label('Published as News')
+                    ->dateTimeTooltip()
+                    ->since()
+                    ->sortable(),
+            ])
+            ->recordActions([
+                DeleteAction::make(),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ManageNews::route('/'),
+        ];
+    }
+
+    public static function getRecordRouteBindingEloquentQuery(): Builder
+    {
+        return parent::getRecordRouteBindingEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
