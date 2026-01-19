545b8276a55c7e7f27e0847fcc05ef4e67763b1b
achyutkneupane <achyutkneupane@gmail.com>
2026-01-19 18:51:47 +0545

    improvements: updates viewport meta tag for better scaling options

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,8 +6,7 @@
         $siteSettings = app(SiteSettings::class);
     @endphp
     <meta charset="utf-8">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=5.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @if($siteSettings->favicon)
         <link rel="shortcut icon" type="image/x-icon" href="{{ asset('/storage/'.$siteSettings->favicon) }}">
