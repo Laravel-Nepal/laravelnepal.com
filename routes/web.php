a9cd074a41fc2e38de2a6dfd3eda1104c5b34814
achyutkneupane <achyutkneupane@gmail.com>
2026-01-29 14:26:30 +0545

    feat: adds Series index page and filtering component Includes routing, controller, and view for series display and filtering.

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\RenderPostIndex;
 use App\Http\Controllers\RenderPostView;
 use App\Http\Controllers\RenderProjectIndex;
 use App\Http\Controllers\RenderProjectView;
+use App\Http\Controllers\RenderSeriesIndex;
 use App\Http\Controllers\RenderTipIndex;
 use App\Http\Controllers\RenderTipView;
 use App\Http\Controllers\SubscribeToNewsletter;
@@ -62,6 +63,21 @@ Route::group([
         ], function (): void {
             Route::get('/', RenderArtisanIndex::class)->name('index');
         });
+    });
+
+    Route::group([
+        'prefix' => '/project',
+        'as' => 'project.',
+    ], function (): void {
+        Route::get('/', RenderProjectIndex::class)->name('index');
+        Route::get('/{project}', RenderProjectView::class)->name('view');
+    });
+
+    Route::group([
+        'prefix' => '/series',
+        'as' => 'series.',
+    ], function (): void {
+        Route::get('/', RenderSeriesIndex::class)->name('index');
     });
 
     Route::get('/open-graph/{model}/{key}', RenderOpenGraphImage::class)->name('openGraphImage');
